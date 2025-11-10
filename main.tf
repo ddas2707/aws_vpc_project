feaccead0a49b4b78be153b02c501f9d3092a8a8
DHRUB DAS <dhrub27das@gmail.com>
2025-11-11 01:56:26 +0530

    Project working only alb remaining

--- main.tf
+++ main.tf
@@ -3,14 +3,14 @@ resource "aws_vpc" "myvpc" {
 }
 
 resource "aws_subnet" "sub1" {
-  vpc_id                  = aws.vpc.vpc_id
+  vpc_id                  = aws_vpc.myvpc.id
   cidr_block              = var.sub1_block
   availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "sub2" {
-  vpc_id                  = aws.vpc.vpc_id
+  vpc_id                  = aws_vpc.myvpc.id
   cidr_block              = var.sub2_block
   availability_zone       = "ap-south-1b"
   map_public_ip_on_launch = true
@@ -27,3 +27,71 @@ resource "aws_route_table" "rt" {
     gateway_id = aws_internet_gateway.igw.id
   }
 }
+
+resource "aws_route_table_association" "rta1" {
+  subnet_id      = aws_subnet.sub1.id
+  route_table_id = aws_route_table.rt.id
+}
+
+resource "aws_route_table_association" "rta2" {
+  subnet_id      = aws_subnet.sub2.id
+  route_table_id = aws_route_table.rt.id
+}
+
+
+resource "aws_security_group" "websg" {
+  name_prefix = "web"
+  vpc_id      = aws_vpc.myvpc.id
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Allow HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "web-sg"
+  }
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "dd-tf-bucket"
+}
+
+resource "aws_instance" "webserver1" {
+  ami                    = "ami-02b8269d5e85954ef"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.sub1.id
+  vpc_security_group_ids = [aws_security_group.websg.id]
+  user_data              = base64encode(file("userdata1.sh"))
+}
+
+resource "aws_instance" "webserver2" {
+  ami                    = "ami-02b8269d5e85954ef"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.sub2.id
+  vpc_security_group_ids = [aws_security_group.websg.id]
+  user_data              = base64encode(file("userdata2.sh"))
+}
+
+
--- variables.tf
+++ variables.tf
@@ -13,7 +13,7 @@ variable "sub1_block" {
 variable "sub2_block" {
   description = "The CIDR block for the subnet2"
   type        = string
-  default     = "10.0.0.0/24"
+  default     = "10.0.1.0/24"
 }
 
 
