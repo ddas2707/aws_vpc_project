3d10a858d811eec8f8c681e7200deaed90be0132
DHRUB DAS <dhrub27das@gmail.com>
2025-11-12 21:47:55 +0530

    project done

--- main.tf
+++ main.tf
@@ -40,19 +40,12 @@ resource "aws_route_table_association" "rta2" {
 
 
 resource "aws_security_group" "websg" {
-  name_prefix = "web"
-  vpc_id      = aws_vpc.myvpc.id
+  name   = "web-sg"
+  vpc_id = aws_vpc.myvpc.id
   ingress {
     description = "Allow HTTP"
     from_port   = 80
     to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    description = "Allow HTTPS"
-    from_port   = 443
-    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -74,8 +67,8 @@ resource "aws_security_group" "websg" {
   }
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "dd-tf-bucket"
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "terraform-project-bucket-das-123562"
 }
 
 resource "aws_instance" "webserver1" {
@@ -94,4 +87,56 @@ resource "aws_instance" "webserver2" {
   user_data              = base64encode(file("userdata2.sh"))
 }
 
+#creating ALB
+resource "aws_lb" "myalb" {
+  name               = "myalb"
+  internal           = false
+  load_balancer_type = "application"
 
+  security_groups = [aws_security_group.websg.id]
+  subnets         = [aws_subnet.sub1.id, aws_subnet.sub2.id]
+
+  tags = {
+    Name = "myalb-web"
+  }
+}
+
+resource "aws_alb_target_group" "tg" {
+  name     = "myTG"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.myvpc.id
+  health_check {
+    path = "/"
+    port = "traffic-port"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "attach1" {
+  target_group_arn = aws_alb_target_group.tg.arn
+  target_id        = aws_instance.webserver1.id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "attach2" {
+  target_group_arn = aws_alb_target_group.tg.arn
+  target_id        = aws_instance.webserver1.id
+  port             = 80
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.myalb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = aws_alb_target_group.tg.arn
+    type             = "forward"
+  }
+}
+
+
+output "loadbalancerdns" {
+  value = aws_lb.myalb.dns_name
+
+}
